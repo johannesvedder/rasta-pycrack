a25fdd8406e557fa68bc0f0fc919aeb3e074486f
Johannes Vedder <johannes.vedder@student.hpi.de>
2022-08-02 16:17:52 +0200

    init

---
+++ main.py
@@ -0,0 +1,110 @@
+import argparse
+import pickle
+
+from scapy.compat import raw
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+from scapy.utils import RawPcapReader
+
+from md4 import MD4
+
+
+def retrieve_safety_code():
+    """
+    Retrieve the first PDU with the safety code of a RaSTA packet
+    :return:
+        rasta_pdu: Hex values of the RaSTA packet PDU
+        safety_code: Hex safety code of the RaSTA packet PDU
+    """
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', '--filepath', help='', required=True)
+    args = vars(parser.parse_args())
+
+    try:
+        pcap_file = RawPcapReader(args['filepath'])
+    except FileNotFoundError:
+        raise FileNotFoundError('Could not find the pcap file')
+
+    print("Analyzing pcap file for RaSTA traffic and saving data to rasta.p")
+
+    for (data, pkt_metadata,) in pcap_file:
+        ether_pkt = Ether(data)
+        if 'type' not in ether_pkt.fields:
+            # LLC frames will have 'len' instead of 'type', disregard them
+            continue
+        if ether_pkt.type != 0x0800:
+            # disregard non-IPv4 packets
+            continue
+        ip_pkt = ether_pkt[IP]
+        if ip_pkt.proto != 17:
+            # ignore non-UDP packet
+            continue
+        udp_pkt = ip_pkt[UDP]
+
+        # todo add more error handling here
+
+        rasta_pkt = raw(udp_pkt.payload)[8:]  # cut out first 8 bytes of redundancy layer
+        rasta_pdu = rasta_pkt[:-8].hex()  # packet without safety code
+        safety_code = rasta_pkt[-8:].hex()  # last 8 bytes safety code
+
+        pdu = bytes.fromhex(rasta_pdu)
+        data = {"pdu": pdu, "code": safety_code}
+        pickle.dump(data, open("rasta.p", "wb"))
+
+        return pdu, safety_code
+
+
+def calculate_safety_code(data, iv):
+    """
+    Calculate the MD4 hash out of the given byte data
+    :param data: The data to calculate the hash
+    :param iv: The IV for the MD4
+    :return: Calculated MD4 hash
+    """
+    md = MD4()
+    md.set_iv(iv)
+    md.add(data)
+    hexhash = md.finish().hex()
+    pdu_hash = hexhash[:16]
+    return pdu_hash
+
+
+def iv_to_msb(iv_string):
+    """
+    Format a hex string to a list of four decimal values in most significant byte order
+    :param iv_string: A hex string (todo how many bytes)
+    :return: The formatted IV list for MD4 initialization
+    """
+    ivlist: list = [iv_string[i:i + 8] for i in range(0, len(iv_string), 8)]
+    for i, s in enumerate(ivlist):
+        barr = bytearray.fromhex(s)
+        sd = bytearray(barr)
+        sd.reverse()
+        ivlist[i] = int(sd.hex(), 16)
+    return ivlist
+
+
+if __name__ == "__main__":
+
+    iv_input = '0123456789ABCDEFFEDCBA9876543210'
+    #iv_input = sys.stdin.readline()
+
+    if iv_input:
+        IV = iv_to_msb(iv_input)
+    else:
+        IV = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476]
+
+    try:
+        pkt_data = pickle.load(open("rasta.p", "rb"))
+        pdu_bytes = pkt_data.get("pdu")
+        safe_code = pkt_data.get("code")
+    except (OSError, IOError, FileNotFoundError) as e:
+        pdu_bytes, safe_code = retrieve_safety_code()
+
+    pdu_hash = calculate_safety_code(pdu_bytes, IV)
+
+    if pdu_hash == safe_code:
+        print("Woop woop hashes are matching!")
+    else:
+        print("ERROR: Hashes not equal")
